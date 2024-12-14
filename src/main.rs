6dfacc59a686f72e3ac49ac957130c72407bb7f0
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-12-14 12:24:03 -0500

    fix(start_sync_test_session): run when entering MatchMaking

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,7 @@ fn main() {
             (
                 spawn_camera,
                 start_matchbox_socket.run_if(|| config::P2P_MODE),
+                start_sync_test_session.run_if(|| !config::P2P_MODE),
             ),
         )
         .add_systems(
@@ -46,11 +47,8 @@ fn main() {
         .add_systems(
             Update,
             (
-                (
-                    create_p2p_session.run_if(|| config::P2P_MODE),
-                    start_sync_test_session.run_if(|| !config::P2P_MODE),
-                )
-                    .run_if(in_state(GameState::MatchMaking)),
+                create_p2p_session
+                    .run_if(in_state(GameState::MatchMaking).and(|| config::P2P_MODE)),
                 handle_ggrs_events.run_if(in_state(GameState::InGame)),
             ),
         )
