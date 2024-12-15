b1d731d3cf0f21ee41e5b7c6a7f21f9cf4fd6cae
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-12-15 18:42:44 -0500

    refactor(monster_movement)

--- src/systems/monster_movement.rs
+++ src/systems/monster_movement.rs
@@ -9,33 +9,42 @@ use bevy::{
 };
 use rand::{rngs::ThreadRng, thread_rng, Rng};
 
-pub fn move_monsters(
-    mut monsters: Query<&mut Transform, With<Monster>>,
-    wall_tiles: Query<&Transform, (With<WallTile>, Without<Monster>)>,
-) {
+type MonsterQuery<'w, 's, 't> = Query<'w, 's, &'t mut Transform, With<Monster>>;
+type WallQuery<'w, 's, 't> = Query<'w, 's, &'t Transform, (With<WallTile>, Without<Monster>)>;
+
+pub fn move_monsters(mut monsters: MonsterQuery, wall_tiles: WallQuery) {
+    let walls = create_wall_set(&wall_tiles);
+    let mut planned = create_current_monster_positions_set(&monsters);
     let mut rng = thread_rng();
-    let walls = HashSet::<DungeonPosition>::from_iter(
-        wall_tiles
+
+    for (mut monster, movement) in monsters
+        .iter_mut()
+        .filter_map(|m| determine_movement(&mut rng).map(|movement| (m, movement)))
+    {
+        let pos = DungeonPosition::from_vec2(monster.translation.truncate() + movement);
+
+        if !planned.contains(&pos) && !walls.contains(&pos) {
+            planned.remove(&DungeonPosition::from_vec3(monster.translation));
+            planned.insert(pos);
+            monster.translation = pos.to_vec3(config::MONSTER_Z_LAYER);
+        }
+    }
+}
+
+fn create_wall_set(walls: &WallQuery) -> HashSet<DungeonPosition> {
+    HashSet::<DungeonPosition>::from_iter(
+        walls
             .iter()
             .map(|w| DungeonPosition::from_vec3(w.translation)),
-    );
-    let mut planned = HashSet::<DungeonPosition>::from_iter(
+    )
+}
+
+fn create_current_monster_positions_set(monsters: &MonsterQuery) -> HashSet<DungeonPosition> {
+    HashSet::<DungeonPosition>::from_iter(
         monsters
             .iter()
             .map(|m| DungeonPosition::from_vec3(m.translation)),
-    );
-
-    for mut monster in &mut monsters {
-        if let Some(movement) = determine_movement(&mut rng) {
-            let pos = DungeonPosition::from_vec2(monster.translation.truncate() + movement);
-
-            if !planned.contains(&pos) && !walls.contains(&pos) {
-                planned.remove(&DungeonPosition::from_vec3(monster.translation));
-                monster.translation = pos.to_vec3(config::MONSTER_Z_LAYER);
-                planned.insert(pos);
-            }
-        }
-    }
+    )
 }
 
 fn determine_movement(rng: &mut ThreadRng) -> Option<Vec2> {
