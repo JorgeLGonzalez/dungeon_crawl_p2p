1f92600e58b2456b0949c87ab061e5cc16b3dbc2
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-12-28 14:16:49 -0500

    refactor(determine_monster_ation): simplify with helper

--- src/systems/monster_actions/determine_monster_action.rs
+++ src/systems/monster_actions/determine_monster_action.rs
@@ -24,42 +24,90 @@ pub fn determine_monster_action(
         return None;
     }
 
-    let movement = determine_movement(rng);
-    let pos_vec2 = current_pos + movement;
-    let pos = DungeonPosition::from_vec2(pos_vec2);
+    let helper =
+        Helper::new(DungeonPosition::from_vec2(current_pos), monster).determine_target_pos(rng);
 
-    if let Some((player, player_id)) = players.get(&pos) {
-        Some(MonsterAction::Attack(MonsterAttacksEvent::new(
-            monster, *player, *player_id, pos_vec2,
-        )))
-    } else if !planned.contains(&pos) && !walls.contains(&pos) {
-        update_plan(planned, current_pos, pos);
-        Some(MonsterAction::Move(MonsterMovesEvent::new(
-            monster,
-            movement,
-            pos_vec2,
-            rng.counter,
-        )))
-    } else {
-        None
-    }
+    helper
+        .attack_player(players)
+        .or_else(|| helper.move_monster(walls, planned))
 }
 
-fn determine_movement(rng: &mut RandomGenerator) -> Vec2 {
-    match rng.gen_range(0..4) {
-        0 => Vec2::Y,
-        1 => Vec2::NEG_Y,
-        2 => Vec2::NEG_X,
-        3 => Vec2::X,
-        _ => unreachable!(),
-    }
-}
-
-fn update_plan(
-    planned: &mut HashSet<DungeonPosition>,
-    current_pos: Vec2,
+struct Helper {
+    current_pos: DungeonPosition,
+    monster: Entity,
+    movement: Vec2,
+    rng_counter: u128,
     target_pos: DungeonPosition,
-) {
-    planned.remove(&DungeonPosition::from_vec2(current_pos));
-    planned.insert(target_pos);
+}
+
+impl Helper {
+    fn new(current_pos: DungeonPosition, monster: Entity) -> Self {
+        Self {
+            current_pos,
+            monster,
+            movement: Vec2::ZERO,
+            rng_counter: 0,
+            target_pos: DungeonPosition::from_vec2(Vec2::ZERO),
+        }
+    }
+
+    fn attack_player(
+        &self,
+        players: &HashMap<DungeonPosition, (Entity, usize)>,
+    ) -> Option<MonsterAction> {
+        players
+            .get(&self.target_pos)
+            .map(|(p, id)| self.create_attack_event(*p, *id))
+            .map(MonsterAction::Attack)
+    }
+
+    fn create_attack_event(&self, player: Entity, player_id: usize) -> MonsterAttacksEvent {
+        MonsterAttacksEvent::new(self.monster, player, player_id, self.target_pos.to_vec2())
+    }
+
+    fn create_move_event(&self) -> MonsterMovesEvent {
+        MonsterMovesEvent::new(
+            self.monster,
+            self.movement,
+            self.target_pos.to_vec2(),
+            self.rng_counter,
+        )
+    }
+
+    fn determine_target_pos(self, rng: &mut RandomGenerator) -> Self {
+        let movement = match rng.gen_range(0..4) {
+            0 => Vec2::Y,
+            1 => Vec2::NEG_Y,
+            2 => Vec2::NEG_X,
+            3 => Vec2::X,
+            _ => unreachable!(),
+        };
+        let target_pos = DungeonPosition::from_vec2(self.current_pos.to_vec2() + movement);
+
+        Self {
+            movement,
+            rng_counter: rng.counter,
+            target_pos,
+            ..self
+        }
+    }
+
+    fn move_monster(
+        &self,
+        walls: &HashSet<DungeonPosition>,
+        planned: &mut HashSet<DungeonPosition>,
+    ) -> Option<MonsterAction> {
+        if planned.contains(&self.target_pos) || walls.contains(&self.target_pos) {
+            return None;
+        }
+
+        self.update_plan(planned);
+
+        Some(MonsterAction::Move(self.create_move_event()))
+    }
+
+    fn update_plan(&self, planned: &mut HashSet<DungeonPosition>) {
+        planned.remove(&self.current_pos);
+        planned.insert(self.target_pos);
+    }
 }
