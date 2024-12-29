4c03b7f3bc09b69ed0c93730b51ade4cf2aa79b9
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-12-28 19:06:32 -0500

    refactor(main): reuse core_systems

--- src/main.rs
+++ src/main.rs
@@ -34,10 +34,63 @@ fn main() {
             }),
         GgrsPlugin::<config::GgrsSessionConfig>::default(),
     ))
-    .init_state::<GameState>();
+    .init_state::<GameState>()
+    .init_resource::<MonsterMoveTracker>();
 
-    app.init_resource::<MonsterMoveTracker>()
-        .add_event::<DesyncEvent>()
+    add_events(&mut app);
+
+    app.add_systems(OnEnter(GameState::Startup), (spawn_camera, startup))
+        .add_systems(
+            OnEnter(GameState::InGame),
+            (spawn_dungeon, spawn_players, spawn_monsters).chain(),
+        )
+        .add_systems(OnEnter(GameState::GameOver), game_over);
+
+    // systems used in both Single Player Update schedule and GgrsScheduled
+    let core_systems = (
+        do_player_action,
+        tick_move_throttle,
+        stop_moving,
+        handle_move_intent,
+        attack_monster,
+        move_player,
+        move_camera,
+        do_monsters_action,
+        attack_player,
+        move_monster,
+    )
+        .chain();
+
+    if game_mode(GameMode::SinglePlayer) {
+        app.add_systems(
+            Update,
+            core_systems.run_if(|| game_mode(GameMode::SinglePlayer)),
+        );
+    } else {
+        ggrs_setup(&mut app);
+
+        app.add_systems(ReadInputs, read_player_inputs)
+            .add_systems(
+                GgrsSchedule,
+                core_systems.run_if(in_state(GameState::InGame)),
+            )
+            .add_systems(GgrsSchedule, persist_monster_moves.after(move_monster))
+            .add_systems(
+                Update,
+                (
+                    create_p2p_session.run_if(
+                        in_state(GameState::Startup).and(|| game_mode(GameMode::MultiPlayer)),
+                    ),
+                    handle_ggrs_events.run_if(in_state(GameState::InGame)),
+                ),
+            );
+    }
+
+    app.run();
+}
+
+fn add_events(app: &mut App) {
+    app.add_event::<DesyncEvent>()
         .add_event::<events::MonsterAttacksEvent>()
         .add_event::<events::MonsterMovesEvent>()
         .add_event::<events::PlayerAttacksEvent>()
@@ -45,8 +98,10 @@ fn main() {
         .add_event::<events::PlayerMoveIntentEvent>()
         .add_event::<events::SnapshotStateEvent>()
         .add_event::<events::StopMovingEvent>();
+}
 
-    // Register components and resources for GGRS snapshots and rollback
+/// Register components and resources for GGRS snapshots and rollback
+fn ggrs_setup(app: &mut App) {
     app
         // .rollback_component_with_clone::<GlobalTransform>()
         // .rollback_component_with_clone::<InheritedVisibility>()
@@ -60,66 +115,6 @@ fn main() {
         .checksum_component::<MoveThrottle>(checksum_move_throttle)
         .checksum_component::<Transform>(checksum_transform)
         .checksum_resource::<RandomGenerator>(checksum_rng);
-
-    app.add_systems(OnEnter(GameState::Startup), (spawn_camera, startup))
-        .add_systems(
-            OnEnter(GameState::InGame),
-            (spawn_dungeon, spawn_players, spawn_monsters).chain(),
-        )
-        .add_systems(
-            Update,
-            (
-                create_p2p_session.run_if(
-                    in_state(GameState::Startup).and(|| GAME_MODE == GameMode::MultiPlayer),
-                ),
-                (
-                    handle_ggrs_events.run_if(
-                        in_state(GameState::InGame).and(|| GAME_MODE != GameMode::SinglePlayer),
-                    ),
-                    (
-                        do_player_action,
-                        tick_move_throttle,
-                        stop_moving,
-                        handle_move_intent,
-                        attack_monster,
-                        move_player,
-                        move_camera,
-                        do_monsters_action,
-                        attack_player,
-                        move_monster,
-                    )
-                        .chain()
-                        .run_if(|| GAME_MODE == GameMode::SinglePlayer),
-                )
-                    .run_if(in_state(GameState::InGame)),
-            ),
-        )
-        .add_systems(ReadInputs, read_player_inputs)
-        .add_systems(
-            GgrsSchedule,
-            (
-                (
-                    // below should follow same order as single player mode Update
-                    do_player_action,
-                    tick_move_throttle,
-                    stop_moving,
-                    handle_move_intent,
-                    attack_monster,
-                    move_player,
-                    move_camera,
-                    do_monsters_action,
-                    attack_player,
-                    move_monster,
-                )
-                    .chain()
-                    .run_if(in_state(GameState::InGame)),
-                persist_monster_moves,
-            )
-                .chain(),
-        )
-        .add_systems(OnEnter(GameState::GameOver), game_over);
-
-    app.run();
 }
 
 #[derive(Clone, Copy, Debug, Default, Eq, Hash, PartialEq, States)]
@@ -129,4 +124,8 @@ enum GameState {
     Paused,
     #[default]
     Startup,
+}
+
+fn game_mode(mode: GameMode) -> bool {
+    GAME_MODE == mode
 }
