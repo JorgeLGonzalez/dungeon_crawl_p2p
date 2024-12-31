d99fe584e535510c00a0853121a06c80fe54e434
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-12-31 16:59:02 -0500

    refactor(Illuminator): factor out of recalculate_fov

--- src/systems/player.rs
+++ src/systems/player.rs
@@ -1,3 +1,5 @@
+mod illuminator;
 mod local_player;
 
+pub use illuminator::{FloorQuery, Illuminator};
 pub use local_player::{LocalPlayer, PlayersQuery};
--- src/systems/recalculate_fov.rs
+++ src/systems/recalculate_fov.rs
@@ -1,19 +1,17 @@
-use super::player::LocalPlayer;
+use super::player::{FloorQuery, Illuminator};
 use crate::{
-    components::{FieldOfView, FloorTile, Player, WallTile},
+    components::{FieldOfView, Player, WallTile},
     events::RecalculateFovEvent,
 };
 use bevy::{prelude::*, utils::hashbrown::HashSet};
 use bevy_ggrs::LocalPlayers;
 
-type FloorQuery<'w, 's, 't, 'r> =
-    Query<'w, 's, (&'t Transform, Entity, &'r mut Sprite), With<FloorTile>>;
 type WallQuery<'w, 's, 't> = Query<'w, 's, &'t Transform, With<WallTile>>;
 
 pub fn recalculate_fov(
     mut fov_query: Query<&mut FieldOfView, With<FieldOfView>>,
     mut recalculate_events: EventReader<RecalculateFovEvent>,
-    mut floor_tiles: FloorQuery,
+    mut floor: FloorQuery,
     local_players: Res<LocalPlayers>,
     players: Query<&Player>,
     walls: WallQuery,
@@ -24,7 +22,7 @@ pub fn recalculate_fov(
         let radius_sq = (fov.radius * fov.radius) as i32;
         let wall_set = create_wall_set(&walls);
 
-        let visible_tiles: Vec<Entity> = floor_tiles
+        let visible_tiles: Vec<Entity> = floor
             .iter()
             .map(|(t, tile, _)| (t.translation.truncate().as_ivec2(), tile))
             .filter(|(floor_pos, _)| entity_pos.distance_squared(*floor_pos) < radius_sq)
@@ -32,14 +30,9 @@ pub fn recalculate_fov(
             .map(|(_, tile)| tile)
             .collect();
 
-        light_floor_for_local_player(
-            &mut floor_tiles,
-            event.entity,
-            &visible_tiles,
-            &fov.visible_tiles,
-            &local_players,
-            &players,
-        );
+        Illuminator::if_local_player(event.entity, &local_players, &players)
+            .with_prior_fov(&fov.visible_tiles)
+            .illuminate(&visible_tiles, &mut floor);
 
         fov.visible_tiles = visible_tiles;
     }
@@ -52,55 +45,22 @@ fn create_wall_set(walls: &WallQuery) -> HashSet<IVec2> {
         .collect()
 }
 
-fn light_floor_for_local_player(
-    floor_tiles: &mut FloorQuery,
-    entity: Entity,
-    fov: &Vec<Entity>,
-    fov_prior: &Vec<Entity>,
-    local_players: &LocalPlayers,
-    players: &Query<&Player>,
-) {
-    let is_local_player = players
-        .get(entity)
-        .is_ok_and(|player| LocalPlayer::is_local(player, &local_players));
-
-    if !is_local_player {
-        return;
-    }
-
-    let mut prior_set: HashSet<Entity> = fov_prior.iter().map(|e| *e).collect();
-
-    fov.iter().for_each(|tile| {
-        if prior_set.contains(tile) {
-            prior_set.remove(tile);
-        } else {
-            let (.., mut sprite) = floor_tiles.get_mut(*tile).expect("Inconceivable!");
-            sprite.color = Color::srgb(0.9, 0.3, 0.5);
-        }
-    });
-
-    prior_set.iter().for_each(|tile| {
-        let (.., mut sprite) = floor_tiles.get_mut(*tile).expect("Inconceivable!");
-        sprite.color = Color::srgb(0.5, 0.3, 0.5);
-    });
-}
-
 /// Use [Bresenham's line algorithm](https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm)
 /// to determine if a wall blocks the line of sight to the given floor tile.
 fn is_visible(entity_pos: IVec2, floor_pos: &IVec2, wall_set: &HashSet<IVec2>) -> bool {
-    let mut entity_x = entity_pos.x;
-    let mut entity_y = entity_pos.y;
+    let mut x = entity_pos.x;
+    let mut y = entity_pos.y;
     let floor_x = floor_pos.x;
     let floor_y = floor_pos.y;
 
-    let x_distance = (floor_x - entity_x).abs();
-    let y_distance = (floor_y - entity_y).abs();
-    let step_x = if entity_x < floor_x { 1 } else { -1 };
-    let step_y = if entity_y < floor_y { 1 } else { -1 };
+    let x_distance = (floor_x - x).abs();
+    let y_distance = (floor_y - y).abs();
+    let step_x = if x < floor_x { 1 } else { -1 };
+    let step_y = if y < floor_y { 1 } else { -1 };
     let mut error_term = x_distance - y_distance;
 
-    while !(entity_x == floor_x && entity_y == floor_y) {
-        if wall_set.contains(&IVec2::new(entity_x, entity_y)) {
+    while !(x == floor_x && y == floor_y) {
+        if wall_set.contains(&IVec2::new(x, y)) {
             return false; // wall obstructs line of sight
         }
 
@@ -108,12 +68,12 @@ fn is_visible(entity_pos: IVec2, floor_pos: &IVec2, wall_set: &HashSet<IVec2>) -
         if e2 > -y_distance {
             // step horizontally since we veered off the vertical line
             error_term -= y_distance;
-            entity_x += step_x;
+            x += step_x;
         }
         if e2 < x_distance {
             // step vertically since we veered off the horizontal line
             error_term += x_distance;
-            entity_y += step_y;
+            y += step_y;
         }
     }
 
