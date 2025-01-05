842d6d57f033709fc58021e71135759232e95208
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2025-01-04 19:05:46 -0500

    fix(MonsterVisibilityToggler): handle local player logic in single player mode

--- src/systems/fov/monster_visibility_toggler.rs
+++ src/systems/fov/monster_visibility_toggler.rs
@@ -2,6 +2,7 @@ use super::fov_queries::FovQuery;
 use crate::{
     components::{FloorTile, FovTileMap, Monster},
     events::FovRecalculationEntityType,
+    systems::player::LocalPlayer,
 };
 use bevy::{prelude::*, utils::hashbrown::HashSet};
 use bevy_ggrs::LocalPlayers;
@@ -45,18 +46,15 @@ impl MonsterVisibilityToggler {
         fov_query: &FovQuery,
         local_players: &LocalPlayers,
     ) {
-        for player_id in local_players.0.iter() {
-            let player_fov: HashSet<IVec2> = fov_query
-                .iter()
-                .find(|(_, player)| player.is_some_and(|p| p.id == *player_id))
-                .map(|(fov, _)| fov.visible_tiles.keys().copied().collect())
-                .expect("Inconceivable!");
+        let player_fov: HashSet<IVec2> = fov_query
+            .iter()
+            .find(|(_, player)| player.is_some_and(|p| LocalPlayer::is_local(p, local_players)))
+            .map(|(fov, _)| fov.visible_tiles.keys().copied().collect())
+            .expect("Inconceivable!");
 
-            let (transform, mut visibility) =
-                monsters.get_mut(self.entity).expect("Inconceivable!");
+        let (transform, mut visibility) = monsters.get_mut(self.entity).expect("Inconceivable!");
 
-            self.toggle_if_needed(&player_fov, transform, &mut visibility);
-        }
+        self.toggle_if_needed(&player_fov, transform, &mut visibility);
     }
 
     fn due_to_player_move(&self, monsters: &mut MonsterQuery, player_fov: HashSet<IVec2>) {
