30dcf3413509e541174136357d7a1a683887b108
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2025-01-11 08:47:41 -0500

    refactor(tooltip): factor out into TooltipController. prelim

--- src/hud/tooltips.rs
+++ src/hud/tooltips.rs
@@ -46,110 +46,190 @@ pub fn tooltip(
     tooltip_entities: Query<(Entity, &TooltipLabel, &Transform)>,
     windows: Query<&Window, With<PrimaryWindow>>,
 ) {
-    let (mut tooltip_node, mut tooltip_text, mut tooltip) = tooltip_ui.single_mut();
-    let (camera, camera_transform) = camera_query.single();
+    let mut ctrl = TooltipController::new(&camera_query, &mut cursor_events, &tooltip_ui, &windows);
+    let result = ctrl.determine(&local_players, &players, &tooltip_entities, &mut tooltip_ui);
 
-    if let Some(entity) = tooltip.entity.as_mut() {
-        if let Some(cursor_position) = windows.single().cursor_position() {
-            if let Ok((.., transform)) = tooltip_entities.get(*entity) {
-                let game_pos = camera
-                    .viewport_to_world_2d(camera_transform, cursor_position)
-                    .expect("Inconceivable!");
-
-                let is_visible = players
-                    .iter()
-                    .find(|(player, _)| LocalPlayer::is_local(player, &local_players))
-                    .map(|(_, fov)| fov)
-                    .expect("No local player to follow!")
-                    .visible_tiles
-                    .contains_key(&game_pos.as_ivec2());
-
-                if !is_visible {
-                    // info!("Cursor no longer in player's FOV");
-                    tooltip_node.display = Display::None;
-                    tooltip_text.0 = String::new();
-                    tooltip.entity = None;
-                    // info!("Entity no longer in player's FOV");
-
-                    return;
-                }
-
-                if hit_test(game_pos, transform) {
-                    // TODO only return if we're still in player's FOV
-                    // info!("Still hovering over tooltip entity");
-                    return;
-                }
-            } else {
-                error!("No transform found for tooltip entity");
-            }
-        } else {
-            error!("No cursor position found");
-        }
-
-        // info!("Entity moved out from under cursor");
-        tooltip_node.display = Display::None;
-        tooltip_text.0 = String::new();
-        tooltip.entity = None;
+    match result {
+        TooltipResult::Hide => ctrl.hide(&mut tooltip_ui),
+        TooltipResult::NoChange => {}
+        TooltipResult::Show => ctrl.show(&hud_camera_query, &mut tooltip_ui),
     }
-
-    let Some(event) = cursor_events.read().last() else {
-        return;
-    };
-
-    let game_pos = camera
-        .viewport_to_world_2d(camera_transform, event.position)
-        .expect("Inconceivable!");
-
-    // TODO skip if delta is too small?
-    // TODO player ID tooltip
-
-    let is_visible = players
-        .iter()
-        .find(|(player, _)| LocalPlayer::is_local(player, &local_players))
-        .map(|(_, fov)| fov)
-        .expect("No local player to follow!")
-        .visible_tiles
-        .contains_key(&game_pos.as_ivec2());
-
-    if !is_visible {
-        // info!("Cursor not in player's FOV");
-        tooltip_node.display = Display::None;
-        tooltip_text.0 = String::new();
-        tooltip.entity = None;
-
-        return;
-    }
-
-    let Some((tooltip_entity, tooltip_label)) = tooltip_entities
-        .iter()
-        .find(|(.., transform)| hit_test(game_pos, transform))
-        .map(|(entity, label, _)| (entity, label.0.clone()))
-    else {
-        // info!("Cursor not over tooltip entity");
-        tooltip_node.display = Display::None;
-        tooltip_text.0 = String::new();
-        tooltip.entity = None;
-
-        return;
-    };
-
-    tooltip_node.display = Display::Block;
-    tooltip_text.0 = tooltip_label;
-    tooltip.entity = Some(tooltip_entity);
-
-    let (hud_camera, hud_transform) = hud_camera_query.single();
-    let ui_pos = hud_camera
-        .viewport_to_world_2d(hud_transform, event.position)
-        .expect("Inconceivable!");
-
-    tooltip_node.left = Val::Px(ui_pos.x);
-    tooltip_node.bottom = Val::Px(ui_pos.y);
 }
 
-fn hit_test(point: Vec2, transform: &Transform) -> bool {
-    let tile_pos = transform.translation.truncate();
-    let min = tile_pos - 0.5;
-    let max = tile_pos + 0.5;
+enum TooltipResult {
+    Hide,
+    NoChange,
+    Show,
+}
 
-    point.x > min.x && point.x < max.x && point.y > min.y && point.y < max.y
+struct TooltipController {
+    game_pos: Option<Vec2>,
+    is_shown: bool,
+    mouse_moved: bool,
+    mouse_pos: Option<Vec2>,
+    target_entity: Option<Entity>,
+    text: String,
+}
+
+impl TooltipController {
+    pub fn new(
+        camera_query: &Query<(&Camera, &GlobalTransform), With<PlayerCamera>>,
+        cursor_events: &mut EventReader<CursorMoved>,
+        tooltip_ui: &Query<(&mut Node, &mut Text, &mut TooltipUI)>,
+        windows: &Query<&Window, With<PrimaryWindow>>,
+    ) -> Self {
+        let (.., tooltip) = tooltip_ui.single();
+        let is_shown = tooltip.entity.is_some();
+
+        let mouse_moved = !cursor_events.is_empty();
+        cursor_events.clear();
+
+        let mouse_pos = windows.single().cursor_position();
+        let game_pos = Self::to_game_pos(mouse_pos, camera_query);
+
+        Self {
+            game_pos,
+            is_shown,
+            mouse_moved,
+            mouse_pos,
+            target_entity: None,
+            text: String::new(),
+        }
+    }
+
+    pub fn determine(
+        &mut self,
+        local_players: &LocalPlayers,
+        players: &Query<(&Player, &FieldOfView)>,
+        tooltip_entities: &Query<(Entity, &TooltipLabel, &Transform)>,
+        tooltip_ui: &mut Query<(&mut Node, &mut Text, &mut TooltipUI)>,
+    ) -> TooltipResult {
+        if self.is_shown {
+            if self.game_pos.is_none() || !self.in_player_fov(local_players, players) {
+                return TooltipResult::Hide;
+            }
+
+            if self.still_on_entity(tooltip_entities, tooltip_ui) {
+                return TooltipResult::NoChange;
+            } // else continue below
+        }
+        // tooltip hidden or shown and no longer over entity
+
+        if self.game_pos.is_none()
+            || !self.in_player_fov(local_players, players)
+            || !self.mouse_moved
+        {
+            return TooltipResult::NoChange;
+        }
+
+        if let Some((tooltip_entity, tooltip_label)) = self.find_entity_to_tooltip(tooltip_entities)
+        {
+            self.target_entity = Some(tooltip_entity);
+            self.text = tooltip_label;
+            return TooltipResult::Show;
+        } else {
+            if self.is_shown {
+                return TooltipResult::Hide;
+            }
+            return TooltipResult::NoChange;
+        }
+    }
+
+    pub fn hide(&self, tooltip_ui: &mut Query<(&mut Node, &mut Text, &mut TooltipUI)>) {
+        let (mut tooltip_node, mut tooltip_text, mut tooltip) = tooltip_ui.single_mut();
+        tooltip_node.display = Display::None;
+        tooltip_text.0 = String::new();
+        tooltip.entity = None;
+    }
+
+    pub fn show(
+        &self,
+        hud_camera_query: &Query<(&Camera, &GlobalTransform), With<HudCamera>>,
+        tooltip_ui: &mut Query<(&mut Node, &mut Text, &mut TooltipUI)>,
+    ) {
+        let target_entity = self.target_entity.expect("Target entity not set!");
+
+        let (mut tooltip_node, mut tooltip_text, mut tooltip) = tooltip_ui.single_mut();
+
+        tooltip_node.display = Display::Block;
+        tooltip_text.0 = self.text.clone();
+        tooltip.entity = Some(target_entity);
+
+        let mouse_pos = self.mouse_pos.expect("MousePos not set!");
+        let (hud_camera, hud_transform) = hud_camera_query.single();
+        let ui_pos = hud_camera
+            .viewport_to_world_2d(hud_transform, mouse_pos)
+            .expect("Inconceivable!");
+
+        tooltip_node.left = Val::Px(ui_pos.x);
+        tooltip_node.bottom = Val::Px(ui_pos.y);
+    }
+
+    fn find_entity_to_tooltip(
+        &self,
+        tooltip_entities: &Query<(Entity, &TooltipLabel, &Transform)>,
+    ) -> Option<(Entity, String)> {
+        tooltip_entities
+            .iter()
+            .find(|(.., transform)| self.hit_test(transform))
+            .map(|(entity, label, _)| (entity, label.0.clone()))
+    }
+
+    fn hit_test(&self, transform: &Transform) -> bool {
+        let Some(point) = self.game_pos else {
+            return false;
+        };
+
+        let tile_pos = transform.translation.truncate();
+        let min = tile_pos - 0.5;
+        let max = tile_pos + 0.5;
+
+        point.x > min.x && point.x < max.x && point.y > min.y && point.y < max.y
+    }
+
+    fn in_player_fov(
+        &self,
+        local_players: &LocalPlayers,
+        players: &Query<(&Player, &FieldOfView)>,
+    ) -> bool {
+        let Some(game_pos) = self.game_pos else {
+            return false;
+        };
+
+        players
+            .iter()
+            .find(|(player, _)| LocalPlayer::is_local(player, &local_players))
+            .map(|(_, fov)| fov)
+            .expect("No local player to follow!")
+            .visible_tiles
+            .contains_key(&game_pos.as_ivec2())
+    }
+
+    fn still_on_entity(
+        &self,
+        tooltip_entities: &Query<(Entity, &TooltipLabel, &Transform)>,
+        tooltip_ui: &mut Query<(&mut Node, &mut Text, &mut TooltipUI)>,
+    ) -> bool {
+        let (.., tooltip) = tooltip_ui.single();
+        let tooltip_entity = tooltip.entity.expect("No active entity!");
+
+        let (.., transform) = tooltip_entities
+            .get(tooltip_entity)
+            .expect("Inconceivable!");
+
+        self.hit_test(transform)
+    }
+
+    fn to_game_pos(
+        mouse_pos: Option<Vec2>,
+        camera_query: &Query<(&Camera, &GlobalTransform), With<PlayerCamera>>,
+    ) -> Option<Vec2> {
+        let (camera, camera_transform) = camera_query.single();
+
+        mouse_pos.map(|pos| {
+            camera
+                .viewport_to_world_2d(camera_transform, pos)
+                .expect("Inconceivable!")
+        })
+    }
 }
