cd4f853d0cf781132f4dca3ca8302d1ae9f2bed1
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2025-02-24 08:47:41 -0500

    test(AStarPathFinder): add unit tests

--- src/dungeon/dungeon_map/cell_automata/a_star_path_finder.rs
+++ src/dungeon/dungeon_map/cell_automata/a_star_path_finder.rs
@@ -65,6 +65,7 @@ impl AStarPathFinder {
     }
 }
 
+#[derive(Debug, Eq, PartialEq)]
 pub(super) enum PathFindingResult {
     /// No path found, but closest position to goal is returned.
     ClosestPos(DungeonPosition),
@@ -72,7 +73,7 @@ pub(super) enum PathFindingResult {
     PathLength(usize),
 }
 
-#[derive(Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq)]
 struct Node {
     pub cost: usize,
     pub pos: DungeonPosition,
@@ -98,4 +99,116 @@ impl PartialOrd for Node {
 
 fn manhattan_distance(a: DungeonPosition, b: DungeonPosition) -> usize {
     (a.x - b.x).abs() as usize + (a.y - b.y).abs() as usize
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::dungeon::config::{X_MIN, Y_MAX, Y_MIN};
+
+    #[test]
+    fn find_no_floors() {
+        let fixture = MapFixture::new(5, 5);
+
+        let result = AStarPathFinder::find(fixture.start(), fixture.map.center, &fixture.map);
+
+        assert_eq!(result, PathFindingResult::ClosestPos(fixture.start()));
+    }
+
+    #[test]
+    fn find_at_goal() {
+        let fixture = MapFixture::new(0, 0);
+
+        let result = AStarPathFinder::find(fixture.start(), fixture.map.center, &fixture.map);
+
+        assert_eq!(result, PathFindingResult::PathLength(1));
+    }
+
+    #[test]
+    fn find_single_path() {
+        let fixture = MapFixture::new(X_MIN, 0).tunnel_east();
+
+        let result = AStarPathFinder::find(fixture.start(), fixture.map.center, &fixture.map);
+
+        assert_eq!(result, PathFindingResult::PathLength(1 + -X_MIN as usize));
+    }
+
+    #[test]
+    fn find_among_many_paths() {
+        let fixture = MapFixture::new(X_MIN, 20).clear_walls();
+
+        let result = AStarPathFinder::find(fixture.start(), fixture.map.center, &fixture.map);
+
+        assert_eq!(
+            result,
+            PathFindingResult::PathLength((fixture.start().y + 1 + -X_MIN) as usize)
+        );
+    }
+
+    #[test]
+    fn find_barred_path() {
+        let x_bar = 10;
+        let fixture = MapFixture::new(20, 20)
+            .clear_walls()
+            .with_vertical_barrier(x_bar);
+
+        let result = AStarPathFinder::find(fixture.start(), fixture.map.center, &fixture.map);
+
+        let closest_pos = DungeonPosition::new(x_bar + 1, fixture.map.center.y);
+        assert_eq!(result, PathFindingResult::ClosestPos(closest_pos));
+    }
+
+    struct MapFixture {
+        pub map: DungeonMap,
+    }
+
+    impl MapFixture {
+        fn new(x: isize, y: isize) -> Self {
+            let mut map = DungeonMap::new();
+
+            let start = DungeonPosition::new(x, y);
+            map.set_tile_type(&start, TileType::Floor);
+            map.player_starting_positions.push(start);
+
+            let center = map.center;
+            map.set_tile_type(&center, TileType::Floor);
+
+            Self { map }
+        }
+
+        fn clear_walls(mut self) -> Self {
+            self.map
+                .tiles()
+                .map(|t| t.pos)
+                .collect::<Vec<_>>()
+                .iter()
+                .for_each(|pos| {
+                    self.map.set_tile_type(&pos, TileType::Floor);
+                });
+
+            self
+        }
+
+        fn start(&self) -> DungeonPosition {
+            self.map.player_starting_positions[0]
+        }
+
+        fn tunnel_east(mut self) -> Self {
+            for x in self.start().x..=self.map.center.x {
+                let pos = DungeonPosition::new(x, self.start().y);
+                self.map.set_tile_type(&pos, TileType::Floor);
+            }
+
+            self
+        }
+
+        fn with_vertical_barrier(mut self, x: isize) -> Self {
+            for y in Y_MIN..=Y_MAX {
+                let pos = DungeonPosition::new(x, y);
+                self.map.set_tile_type(&pos, TileType::Wall);
+            }
+
+            self
+        }
+    }
 }
