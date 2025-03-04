83d61edc430a4bbdc0b4b507d6791ec494738ae6
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2025-03-03 19:26:50 -0500

    test(BlueprintTile)

--- src/dungeon/dungeon_map/prefab/blueprint_tile.rs
+++ src/dungeon/dungeon_map/prefab/blueprint_tile.rs
@@ -7,7 +7,7 @@ use crate::{
 
 /// A blueprint tile specification for a dungeon prefab in a specified dungeon
 /// position.
-#[derive(Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq)]
 pub enum BlueprintTile {
     Floor(DungeonPosition),
     Item(DungeonPosition),
@@ -90,5 +90,141 @@ impl BlueprintTile {
             BlueprintTile::Wall(_) => TileType::Wall,
             _ => TileType::Floor,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case::floor('-', BlueprintTile::Floor(center()))]
+    #[case::wall('#', BlueprintTile::Wall(center()))]
+    #[case::item('I', BlueprintTile::Item(center()))]
+    #[case::monster('M', BlueprintTile::Monster(center()))]
+    #[case::ogre('O', BlueprintTile::Ogre(center()))]
+    #[case::huge_sword('S', BlueprintTile::Sword(center()))]
+    #[case::magic_map('P', BlueprintTile::Map(center()))]
+    #[case::key_marker('X', BlueprintTile::KeyMarker(center()))]
+    fn blueprint_tile(#[case] tile_char: char, #[case] expected: BlueprintTile) {
+        assert_eq!(BlueprintTile::new(tile_char, expected.pos()), expected);
+    }
+
+    #[test]
+    #[should_panic(expected = "Unknown tile type: ','")]
+    fn invalid_tile_symbol() {
+        BlueprintTile::new(',', center());
+    }
+
+    #[test]
+    fn test_floor_tile() {
+        let mut map = DungeonMap::new();
+        let pos = center();
+
+        BlueprintTile::Floor(pos).add_to(&mut map);
+
+        assert_eq!(map.get_tile_type(&pos), TileType::Floor);
+        assert!(map.item_positions.is_empty());
+        assert!(map.monster_starting_positions.is_empty());
+    }
+
+    #[test]
+    fn test_wall_tile() {
+        let mut map = DungeonMap::new();
+        let pos = center();
+
+        BlueprintTile::Wall(pos).add_to(&mut map);
+
+        assert_eq!(map.get_tile_type(&pos), TileType::Wall);
+        assert!(map.item_positions.is_empty());
+        assert!(map.monster_starting_positions.is_empty());
+    }
+
+    #[test]
+    fn test_item_placement() {
+        let mut map = DungeonMap::new();
+        let pos = center();
+
+        BlueprintTile::Item(pos).add_to(&mut map);
+
+        assert_eq!(map.get_tile_type(&pos), TileType::Floor);
+        assert_eq!(map.item_positions.len(), 1);
+        assert_eq!(map.item_positions[0].pos, pos);
+        assert_eq!(map.item_positions[0].item, None);
+    }
+
+    #[test]
+    fn test_monster_placement() {
+        let mut map = DungeonMap::new();
+        let pos = center();
+
+        BlueprintTile::Monster(pos).add_to(&mut map);
+
+        assert_eq!(map.get_tile_type(&pos), TileType::Floor);
+        assert_eq!(map.monster_starting_positions.len(), 1);
+        assert_eq!(map.monster_starting_positions[0].pos, pos);
+        assert_eq!(map.monster_starting_positions[0].monster, None);
+    }
+
+    #[test]
+    fn test_ogre_placement() {
+        let mut map = DungeonMap::new();
+        let pos = center();
+
+        BlueprintTile::Ogre(pos).add_to(&mut map);
+
+        assert_eq!(map.get_tile_type(&pos), TileType::Floor);
+        assert_eq!(map.monster_starting_positions.len(), 1);
+        assert_eq!(map.monster_starting_positions[0].pos, pos);
+        assert_eq!(
+            map.monster_starting_positions[0].monster,
+            Some(Monster::Orc)
+        );
+    }
+
+    #[test]
+    fn test_huge_sword_placement() {
+        let mut map = DungeonMap::new();
+        let pos = center();
+
+        BlueprintTile::Sword(pos).add_to(&mut map);
+
+        assert_eq!(map.get_tile_type(&pos), TileType::Floor);
+        assert_eq!(map.item_positions.len(), 1);
+        assert_eq!(map.item_positions[0].pos, pos);
+        assert_eq!(
+            map.item_positions[0].item,
+            Some(MagicItem::Weapon(Weapon::HugeSword))
+        );
+    }
+
+    #[test]
+    fn test_map_placement() {
+        let mut map = DungeonMap::new();
+        let pos = center();
+
+        BlueprintTile::Map(pos).add_to(&mut map);
+
+        assert_eq!(map.get_tile_type(&pos), TileType::Floor);
+        assert_eq!(map.item_positions.len(), 1);
+        assert_eq!(map.item_positions[0].pos, pos);
+        assert_eq!(map.item_positions[0].item, Some(MagicItem::Map));
+    }
+
+    #[test]
+    fn test_key_marker() {
+        let mut map = DungeonMap::new();
+        let pos = center();
+
+        BlueprintTile::KeyMarker(pos).add_to(&mut map);
+
+        assert_eq!(map.get_tile_type(&pos), TileType::Floor);
+        assert!(map.item_positions.is_empty());
+        assert!(map.monster_starting_positions.is_empty());
+    }
+
+    fn center() -> DungeonPosition {
+        DungeonPosition::new(0, 0)
     }
 }
